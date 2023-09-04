0b26a495a6862307eb2363f4c37740c9c15b3526
Fabricio Floriano <f.florian0@hotmail.com>
2023-09-04 05:16:20 -0300

    Requisito 05

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import productController from '../controllers/product.controller';
+import validationName from '../middlewares/name.validations';
+import validationPrice from '../middlewares/price.validations';
 
 const productRouter = express.Router();
 
-productRouter.post('/', productController.createProduct);
+productRouter.post('/', validationName, validationPrice, productController.createProduct);
 productRouter.get('/', productController.getAllProducts);
 
 export default productRouter;
